49ea92d9d3264e4abce8e5b5daa586d90b3707c2
Gabrielle Naidoo <naidoogabrielle2@gmail.com>
2023-11-21 23:54:17 +0200

    Addition of dropdown menu for genre filtering

--- vite-project/App.jsx
+++ vite-project/App.jsx
@@ -1,28 +1,77 @@
 import { useState, useEffect } from "react";
-import {
-  Link,
-  Route,
-  Routes,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+import { Route, Routes, NavLink } from "react-router-dom";
 import Preview from "/components/Preview";
 import Show from "/components/Show";
 import Episodes from "/components/Episodes";
 import Favourites from "/components/Favourites";
 import Dropdown from "/components/Dropdown";
-import GenreSorted from "/components/GenreSorted";
+import GenreDropdown from "/components/GenreDropdown";
 
 function App() {
   const [podcastData, setPodcastData] = useState([]);
-  const [formData, setFormData] = useState({ selectedValue: "all" });
-  const navigate = useNavigate();
-  const location = useLocation();
+  const [formData, setFormData] = useState({
+    selectedValueFilter: "all",
+    selectedGenreFilter: "all",
+  });
 
   function handleChange(event) {
-    const { value } = event.target;
-    setFormData({ selectedValue: value });
+    const { value, name } = event.target;
+    // setFormData({ selectedValueFilter: value });
+    setFormData((prev) => ({ ...prev, [name]: value }));
   }
+
+  // function handleGenreSelect(podcastData) {
+  //   if (formData.selectedGenreFilter === "personalGrowth") {
+  //     const personalGrowthArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(1);
+  //     });
+  //     setPodcastData([...personalGrowthArray]);
+  //   } else if (
+  //     formData.selectedGenreFilter === "trueCrimeAndInvestigativeJournalism"
+  //   ) {
+  //     const trueCrimeAndInvestigativeJournalismArray = [...podcastData].filter(
+  //       (podcast) => {
+  //         return podcast.genres.includes(2);
+  //       }
+  //     );
+  //     setPodcastData([...trueCrimeAndInvestigativeJournalismArray]);
+  //   } else if (formData.selectedGenreFilter === "history") {
+  //     const historyArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(3);
+  //     });
+  //     setPodcastData([...historyArray]);
+  //   } else if (formData.selectedGenreFilter === "comedy") {
+  //     const comedyArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(4);
+  //     });
+  //     setPodcastData([...comedyArray]);
+  //   } else if (formData.selectedGenreFilter === "entertainment") {
+  //     const entertainmentArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(5);
+  //     });
+  //     setPodcastData([...entertainmentArray]);
+  //   } else if (formData.selectedGenreFilter === "business") {
+  //     const businessArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(6);
+  //     });
+  //     setPodcastData([...businessArray]);
+  //   } else if (formData.selectedGenreFilter === "fiction") {
+  //     const fictionArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(7);
+  //     });
+  //     setPodcastData([...fictionArray]);
+  //   } else if (formData.selectedGenreFilter === "news") {
+  //     const newsArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(8);
+  //     });
+  //     setPodcastData([...newsArray]);
+  //   } else if (formData.selectedGenreFilter === "kidsAndFamily") {
+  //     const kidsAndFamilyArray = [...podcastData].filter((podcast) => {
+  //       return podcast.genres.includes(9);
+  //     });
+  //     setPodcastData([...kidsAndFamilyArray]);
+  //   }
+  // }
 
   useEffect(() => {
     fetch("https://podcast-api.netlify.app/shows")
@@ -34,24 +83,24 @@ function App() {
       })
       .then((data) => {
         // conditionals based on filtering for podcastData
-        if (formData.selectedValue === "alphabetical") {
+        if (formData.selectedValueFilter === "alphabetical") {
           const alphabeticalPodcastData = [...podcastData].sort((a, b) =>
             a.title.toLowerCase().localeCompare(b.title.toLowerCase())
           );
           setPodcastData([...alphabeticalPodcastData]);
-        } else if (formData.selectedValue === "reverse-alphabetical") {
+        } else if (formData.selectedValueFilter === "reverse-alphabetical") {
           const reverseAlphabeticalPodcastData = [...podcastData].sort((a, b) =>
             b.title.toLowerCase().localeCompare(a.title.toLowerCase())
           );
           setPodcastData([...reverseAlphabeticalPodcastData]);
-        } else if (formData.selectedValue === "newest-to-oldest") {
+        } else if (formData.selectedValueFilter === "newest-to-oldest") {
           const newestToOldestPodcastData = [...podcastData].sort((a, b) => {
             const aDate = new Date(a.updated) || 0;
             const bDate = new Date(b.updated) || 0;
             return bDate - aDate;
           });
           setPodcastData([...newestToOldestPodcastData]);
-        } else if (formData.selectedValue === "oldest-to-newest") {
+        } else if (formData.selectedValueFilter === "oldest-to-newest") {
           const oldestToNewestPodcastData = [...podcastData].sort((a, b) => {
             const aDate = new Date(a.updated) || 0;
             const bDate = new Date(b.updated) || 0;
@@ -63,7 +112,7 @@ function App() {
         }
       })
       .catch((err) => console.log(err));
-  }, [formData.selectedValue, podcastData]);
+  }, [formData.selectedValueFilter, podcastData]);
 
   const podcastElement = podcastData.map((element) => {
     return (
@@ -73,26 +122,34 @@ function App() {
     );
   });
 
-  const isFavouritesPage = location.pathname === "/favourites";
-
   return (
     <>
-      <nav style={{ display: "flex", justifyContent: "space-between" }}>
-        <h1>PodPortal</h1>
-        <Link to="/">
-          <h1>Podcasts</h1>
-        </Link>
-        <Link to="/favourites">
+      <nav
+        className="navbar"
+        style={{ display: "grid", gridTemplateColumns: "2fr 1fr 1fr 1fr 2fr" }}
+      >
+        <h1 style={{ gridColumn: "1/3" }}>PodPortal</h1>
+        <NavLink to="/" style={{ gridColumn: "3/4" }}>
+          <h1>All Podcasts</h1>
+        </NavLink>
+        <NavLink to="/favourites" style={{ gridColumn: "4/5" }}>
           <h1>Favourites</h1>
-        </Link>
-        {!isFavouritesPage && (
-          <Dropdown data={formData} changeHandler={handleChange} />
-        )}
+        </NavLink>
       </nav>
       <Routes>
         <Route
           path="/"
-          element={<div className="previews-container">{podcastElement}</div>}
+          element={
+            <>
+              <div>
+                <Dropdown data={formData} changeHandler={handleChange} />
+              </div>
+              <div>
+                <GenreDropdown data={formData} changeHandler={handleChange} />
+              </div>
+              <div className="previews-container">{podcastElement}</div>
+            </>
+          }
         />
         <Route path="/show/:id" element={<Show />} />
         <Route path="/show/:id/season/:seasonNumber" element={<Episodes />} />
--- vite-project/components/DropDown.jsx
+++ vite-project/components/DropDown.jsx
@@ -2,18 +2,30 @@ import React from "react";
 
 function Dropdown(props) {
   return (
-    <select
-      id="selected"
-      value={props.data.selectedValue}
-      onChange={props.changeHandler}
-      name="selected"
-    >
-      <option value="all">All</option>
-      <option value="alphabetical">A-Z</option>
-      <option value="reverse-alphabetical">Z-A</option>
-      <option value="newest-to-oldest">Newest to oldest</option>
-      <option value="oldest-to-newest">oldest to newest</option>
-    </select>
+    <div className="dropdown-main-page">
+      <label
+        htmlFor="selectedFilter"
+        style={{ marginRight: "1rem", fontSize: "1.5rem" }}
+      >
+        Filter by:
+      </label>
+      <select
+        id="selectedFilter"
+        value={props.data.selectedValueFilter}
+        onChange={props.changeHandler}
+        name="selectedValueFilter"
+        style={{
+          fontSize: "1rem",
+          textAlign: "center",
+        }}
+      >
+        <option value="all">All</option>
+        <option value="alphabetical">A-Z</option>
+        <option value="reverse-alphabetical">Z-A</option>
+        <option value="newest-to-oldest">Newest to oldest</option>
+        <option value="oldest-to-newest">oldest to newest</option>
+      </select>
+    </div>
   );
 }
 
